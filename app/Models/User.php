944906b017df809137273b7b27f985e379693bb5
andrewnaiemf <andrewnaiemf@gmail.com>
2024-02-29 15:07:11 +0200

    fix device token issue  in branches

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,6 +51,25 @@ class User extends Authenticatable implements JWTSubject
         'email_verified_at' => 'datetime',
     ];
 
+
+    public function getDeviceTokenAttribute($value)
+    {
+        // Check if the value is already an array
+        if (is_array($value)) {
+            return $value;
+        }
+
+        // Check if the value is a JSON string
+        if (is_string($value) && json_decode($value) !== null) {
+            return json_decode($value, true);
+        }
+
+        // If none of the above conditions match, return an empty array
+        return [];
+
+    }
+    
+
     /**
      * Get the identifier that will be stored in the subject claim of the JWT.
      *
