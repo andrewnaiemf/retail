c9f6c2103af39422173f5c038968896cf04e7023
andrewnaiemf <andrewnaiemf#gmail.com>
2024-03-09 16:06:26 +0200

    fix payment_method in  invoice

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -30,7 +30,8 @@ class Invoice extends Model
     {
         $invoice = parent::toArray();
         $invoice['status'] = $this->status == 'Paid' ? trans('locale.Paid') : $this->status ;
-        $invoice['payment_method'] = $this->payment_method == -1 ? trans('locale.in_advance') : $this->status ;
+        $invoice['payment_method'] = $this->due_amount == 0 ? trans('locale.paid') : ($this->payment_method == -1 ? trans('locale.in_advance') : $this->status);
+
         $invoice['inventory'] = Inventory::find(1);
         $invoice = array_merge($invoice, ['type' => trans('locale.tax_invoice'), 'owner' => $this->getOwnerAttribute()]);
         return $invoice;
