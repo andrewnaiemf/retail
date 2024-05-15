b60af7d6cdde8ffa18e46bb5b17e1c28d010aef1
andrewnaiemf <andrewnaiemf#gmail.com>
2024-05-15 17:58:51 +0300

    retranslate received order notification

--- resources/lang/ar/messages.php
+++ resources/lang/ar/messages.php
@@ -8,7 +8,7 @@ return [
     'your_order_declined' => 'تم رفض طلبك',
     'your_order_approved' => 'تم الموافقة على طلبك',
     'your_order_processing' => 'جاري تحضير الطلب',
-    'your_order_received' => 'تم تسليم الطلب للكابتن',
+    'your_order_received' => 'تم تسليم الطلب لمسؤل المبيعات',
     'your_order_delivery' => 'طلبك في الطريق اليك',
     'your_order_delivered' => 'تم توصيل الطلب',
 
